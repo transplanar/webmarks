7cbd52a8c6a916e5acdb2cb0da0a2b726882e946
transplanar <glen@glencooney.com>
2015-12-29 22:49:10 -0500

    Set up rspec, initial specs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # REVIEW is this correct? better alternative to all these get statements?
   # get 'home/index'
 
+  # REVIEW advantage/convention for each way of doing this?
   # get 'home/about'
+  get 'about' => 'home#about'
 
   devise_for :users
   resources :users
