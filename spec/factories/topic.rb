7e6654e8b7a2936c4dbd0c045de46775830e933a
transplanar <glen@glencooney.com>
2016-01-03 16:50:17 -0500

    Disable factories for now to prevent errors

--- spec/factories/topic.rb
+++ spec/factories/topic.rb
@@ -1,8 +1,6 @@
-include Faker
-
-FactoryGirl.define do
-  factory :topic do
-      title Faker::Lorem:sentence
-      topic
-  end
-end
+# FactoryGirl.define do
+#   factory :topic do
+#       title Faker::Lorem:sentence
+#       topic
+#   end
+# end
