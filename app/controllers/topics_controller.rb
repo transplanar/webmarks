84c2a65d8e376a60427ec3ac769d1d825bd974c6
transplanar <glen@glencooney.com>
2016-01-03 18:41:23 -0500

    Bookmark edit and delete functionality (with buttons)

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,5 @@
 class TopicsController < ApplicationController
-  # before_action :set_topic, only: [:show, :edit, :update, :destroy]
+  before_action :set_topic, only: [:show, :edit, :update, :destroy]
 
   def index
     @topics = Topic.all
@@ -24,7 +24,7 @@ class TopicsController < ApplicationController
 
     if @topic.save
       flash[:notice] = "Topic \##{@log} was successfully created."
-      redirect_to bookmarks_path
+      redirect_to root_path
     else
       render :new
     end
@@ -45,15 +45,15 @@ class TopicsController < ApplicationController
     set_log(@topic)
 
     @topic.destroy
-    redirect_to bookmarks_url
+    redirect_to root_path
     flash[:notice] = "Topic \##{@log} was successfully destroyed."
   end
 
   private
-    # def set_topic
-    #   # @topic = Topic.find(params[:id])
-    #   @topic = Topic.find(params[:topic_id])
-    # end
+    def set_topic
+      @topic = Topic.find(params[:id])
+      # @topic = Topic.find(params[:topic_id])
+    end
 
     def topic_params
       params.require(:topic).permit(:title, :user_id)
