87ab0b3264adf777497ed3850796aab4dbede6d3
transplanar <glen@glencooney.com>
2016-01-05 18:34:09 -0500

    Pundit implemented. Just requires additional user implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  include Pundit
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = 'You are not authorized to perform this action.'
+    redirect_to(request.referrer || root_path)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    # REVIEW: what is this for?
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
 end
