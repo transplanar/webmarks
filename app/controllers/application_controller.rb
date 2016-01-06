fccb5ac1f8c9fba6693e75ab44162a74d9b13d1e
transplanar <glen@glencooney.com>
2016-01-05 22:30:23 -0500

    Bookmark authorization implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   private
 
   def user_not_authorized
+    # TODO make this more descriptive of why rejected
     flash[:alert] = 'You are not authorized to perform this action.'
     redirect_to(request.referrer || root_path)
   end
--- app/policies/bookmark_policy.rb
+++ app/policies/bookmark_policy.rb
@@ -1,3 +1,15 @@
 class BookmarkPolicy < ApplicationPolicy
+  def update?
+    # false
+    user.present? && (record.topic.user == user)
+  end
 
+  def edit?
+    update?
+  end
+
+  def destroy?
+    # false
+    user.present? && (record.topic.user == user)
+  end
 end
