1b2e6aae01d7c51b46976e4e4b9a1e5d80cdd97c
transplanar <glen@glencooney.com>
2016-01-05 23:10:35 -0500

    Start of like/unlike implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,11 @@ class User < ActiveRecord::Base
   has_many :topics
 
   # REVIEW is this correct?
-  has_many :bookmarks, through: :topics
+  # has_many :bookmarks, through: :topics
+  # has_many :bookmarks
+  has_many :likes, dependent: :destroy
+
+  def liked(bookmark)
+    likes.where(bookmark_id: bookmark.id).first
+  end
 end
