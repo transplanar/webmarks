98f074821c4ab1f14b9a71f7ab3937edbe928dad
transplanar <glen@glencooney.com>
2016-01-12 16:38:13 -0500

    Append .first to where call for incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,14 +3,14 @@ class IncomingController < ApplicationController
 
   # TODO need to verify whether topic is new or not
   def create
-    user = User.where(email: params[:sender])
+    user = User.where(email: params[:sender]).first
 
     # REVIEW what to do with user that has no login info?
     if (user.nil?)
       user = User.create!(email: params[:sender])
     end
 
-    topic = Topic.where(title: params[:subject])
+    topic = Topic.where(title: params[:subject]).first
 
     if (topic.nil?)
       topic = Topic.create!(title: params[:subject], user: user)
@@ -19,7 +19,7 @@ class IncomingController < ApplicationController
     # bookmark = topic.bookmarks.create!(url: params["body-plain"])
     # topic.bookmarks.create!(url: params["body-plain"])
 
-    Bookmark.create!(url: params["body-plain"], topic: topic) 
+    Bookmark.create!(url: params["body-plain"], topic: topic)
 
     # topic.bookmarks.create!(url: params["body-plain"])
 
